ac9fccbb28b71943e95908460348f1da5a19905d
flip4fzL <68507887+flip4fzL@users.noreply.github.com>
2021-10-14 14:15:37 -0500

    Add files via upload

---
+++ fr.lua
@@ -0,0 +1,10 @@
+Locale                          = Locale or {}
+
+Locale.fr = { -- fr is the reference that will be used for 'Config.Language'
+	StandaloneLapText			= "~r~E~w~ - Demander un lap dance", -- Définit le texte qui sera affiché au-dessus du marqueur si 'Config.Framework' est mis sur 'standalone'
+	LapText						= "~r~E~w~ - Acheter un lap dance (~g~" .. Config.LapDanceCost .. "$~w~)", -- Définit le texte qui sera affiché au-dessus du marqueur
+	BoughtLapdance				= "Vous avez acheté un lap dance pour $".. Config.LapDanceCost, -- Texte de la notification lorsqu'un lap dance est acheté
+	StripperPause				= "La stripteaseuse a besoin de repos !", -- Texte de la notification si un joueur veut directement racheter un lapdance dans les 10 secondes après avoir fini son dernier
+	StripperActive				= "La strip-teaseuse est déjà occupé !", -- Texte de la notification si une strip-teaseuse est déjà occupé lorsque vous essayez d'acheter un lap dance
+	NotEnoughMoney				= "Vous n'avez pas assez d'argent. Un lap dance coûte $".. Config.LapDanceCost -- Texte de la notification si le joueur n'a pas assez d'argent
+}
